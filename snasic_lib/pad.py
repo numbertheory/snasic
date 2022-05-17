734d8850bb297fe8df26003c1dcad3454a551f22
JP Etcheber <jetcheber@gmail.com>
2022-05-17 11:21:32 -0700

    refresh for pad update

--- snasic_lib/pad.py
+++ snasic_lib/pad.py
@@ -10,8 +10,12 @@ class Pad:
         self.pad.addstr(0, 0, content)
         self.refresh()
 
-    def refresh(self):
-        self.pad.refresh(0, 0, 0, 0, 10, 10)
+    def refresh(self, ulp=(0, 0), ulw=(0, 0), lrw=(10, 10)):
+        # A pad contains a window, which contains the text
+        # ulp - upper-left coordinate of the pad area (relative to screen)
+        # ulw - upper-left coordinate of the window area (relative to the pad)
+        # lrw - lower-right coordinate of the window area (relative to the pad)
+        self.pad.refresh(ulp[0], ulp[1], ulw[0], ulw[1], lrw[0], lrw[1])
 
     def clear(self):
         pass
--- snasic_lib/run_basic_program.py
+++ snasic_lib/run_basic_program.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 from snasic_lib.screen import Screen
 from snasic_lib.pad import Pad
+from snasic_lib.load_file import load_structured_basic_file
 
 
 def start_program(stdscr, args):
     pad = Pad()
     screen = Screen(stdscr, args)
+    basic_script, numbered_lines = load_structured_basic_file(
+        args.filename
+    )
     while True:
         pad.printpad("hello")
         pad.refresh()
